9c8e6645d6e53a5fd52be2c9c836115e9a47c6b4
Suresh Pachari <pacharipallisuresh@gmail.com>
2026-02-02 05:17:43 +0530

    passing arguments

--- variables.sh
+++ variables.sh
@@ -4,7 +4,7 @@
 
 # case:1
 
-echo "variable case:1"
+echo "variable initilization case:1"
 
 PERSON1=Suresh # no space between value and =
 PERSON2=Ramesh
@@ -14,7 +14,7 @@ echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
 echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
 echo "$PERSON2:: I am learning Shell. What about you?"
 
-echo "variables case:2"
+echo "variables case:2 passing arguments"
 
 PERSON1=$1 
 PERSON2=$2
@@ -23,3 +23,15 @@ echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
 echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
 echo "$PERSON2:: I am learning Shell. What about you?"
+
+# read command use cases in shellscript
+
+echo "read command use case:3"
+
+echo "please enter your name:"
+read username
+echo "your name is : $username"
+
+echo "please enter your password:"
+read -s password
+echo "entered password: $password"
