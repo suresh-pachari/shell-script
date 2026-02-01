d537aa94051e4f64eb0572c24bc267b06584db65
Suresh Pachari <pacharipallisuresh@gmail.com>
2026-02-02 05:04:39 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -2,10 +2,24 @@
 
 # This is comment in the shell script it will not exicuted by bash.
 
-echo "Hello World I am yours Suresh"
+# case:1
 
-echo " HiI am learning Git"
+echo "variable case:1"
 
-echo "this is to test the modifications to refleact git"
+PERSON1=Suresh # no space between value and =
+PERSON2=Ramesh
 
-echo "basics of the git how it works explained by shiva was good"
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell. What about you?"
+
+echo "variables case:2"
+
+PERSON1=$1 
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell. What about you?"
