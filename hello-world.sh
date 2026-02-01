e210fbca081abe81cfe751fbec6c536b73ee8acf
Suresh Pachari <pacharipallisuresh@gmail.com>
2026-02-02 04:18:33 +0530

    modified something

--- hello-world.sh
+++ hello-world.sh
@@ -7,3 +7,5 @@ echo "Hello World I am yours Suresh"
 echo " HiI am learning Git"
 
 echo "this is to test the modifications to refleact git"
+
+echo "basics of the git how it works explained by shiva was good"
