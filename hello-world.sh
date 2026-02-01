2319eaf9c81dd5216e5386782903bf67a4caa328
Suresh Pachari <pacharipallisuresh@gmail.com>
2026-02-02 03:32:04 +0530

    changed something

--- hello-world.sh
+++ hello-world.sh
@@ -3,3 +3,7 @@
 # This is comment in the shell script it will not exicuted by bash.
 
 echo "Hello World I am yours Suresh"
+
+echo " HiI am learning Git"
+
+echo "this is to test the modifications to refleact git"
