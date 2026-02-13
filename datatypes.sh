152df4e9f77bab06efd8106b0b763f6b224b629b
Suresh Pachari <pacharipallisuresh@gmail.com>
2026-02-13 22:55:54 +0530

    sum of variables

--- datatypes.sh
+++ datatypes.sh
@@ -6,3 +6,11 @@ NUM2=Soori
 SUM=$(($NUM2+$NUM1))
 
 echo "total sum is : $SUM"
+
+# Array
+FRUITS=("Apple" "Banana" "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
