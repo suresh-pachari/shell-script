e51b2a193f2aa1545a44c5f8a3f07fa2281c4915
Suresh Pachari <pacharipallisuresh@gmail.com>
2026-02-13 23:20:16 +0530

    conditons check

--- conditions.sh
+++ conditions.sh
@@ -7,16 +7,15 @@ Number=$1
 # -eq ---> equal to
 # -nq ---> not equal to
 
-if [ $Number -gt 20 ] then
-do 
+if [ $Number -gt 20 ]; then
+
     echo "$Number is greater than 20"
 
-elif [ $Number -eq 20 ]
+elif [ $Number -eq 20 ];
 
     echo "$Number is equal to 20"
 
-else [ $Number -lt 20 ]
-
+else 
     echo "$Number is lessthan 20"
 
-done
+fi
